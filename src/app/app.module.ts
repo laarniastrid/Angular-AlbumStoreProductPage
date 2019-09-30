5c0d4f90d21d11bef51b5da528f26e3847cea245
Astrid Sutton <astrid.sutton@wgu.edu>
2019-09-30 15:24:51 -0600

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,19 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductPageComponent,
-    ProductDescriptionComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [
-	  ProductService,
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ProductPageComponent,
+		ProductDescriptionComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule
+	],
+	providers: [
+		ProductService,
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
