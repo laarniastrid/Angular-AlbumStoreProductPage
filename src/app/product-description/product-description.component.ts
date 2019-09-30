5d3de9d190054925c9e4cf9a0081cac0007ddb8d
Astrid Sutton <astrid.sutton@wgu.edu>
2019-09-30 15:36:12 -0600

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { ProductService } from 'app/product.service';
 
 @Component({
@@ -9,13 +10,10 @@ import { ProductService } from 'app/product.service';
 export class ProductDescriptionComponent implements OnInit {
 	albumInfo;
 
-	constructor(
-		private _productService: ProductService,
-	) { }
+	constructor(private _productService: ProductService) { }
 
 	ngOnInit() {
-		this._productService.getAlbum()
-			.subscribe(response => this.albumInfo = response)
+		this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
 	}
 
 }
